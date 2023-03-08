2b9a74ce3f2710ffce2bacdb713096be7db0acb3
Paul <paul.huffman97@outlook.com>
2023-03-08 17:20:02 -0500

    Reviews now work, and are shown on the detail page

--- app.js
+++ app.js
@@ -30,6 +30,17 @@ const albums={
 			deleteButton.addEventListener('click',function(){
 				database.delete(albums.documentID,index);
 			});
+
+			// loop through reviews array and create new HTML element for each review
+			let reviewsDiv = document.getElementById('reviews');
+			if (item.reviews) {
+				for (let i = 0; i < item.reviews.length; i++) {
+					let review = item.reviews[i];
+					let reviewDiv = document.createElement('div');
+					reviewDiv.innerHTML = `<p>${review.name}: ${review.text}</p>`;
+					reviewsDiv.appendChild(reviewDiv);
+				}
+			}
 		});
 	},
 	create:function(){
@@ -67,31 +78,42 @@ const albums={
 			});
 		});
 	},
-	review:function(index){
-		database.detail(albums.documentID,index,function(item){
-			document.getElementById('loading').style.display='none';
-			document.getElementById('album-band').innerText=item.band;
-			document.getElementById('album-text').innerText=item.album;
-			document.querySelector('form input[name=name]').value=item.review.name;
-			document.querySelector('form textarea[name=review]').value=item.review.text;
-			
-			document.querySelector('form').addEventListener('submit',function(e){
-				e.preventDefault();
-				let band=document.querySelector('form input[name=band]');
-				let album=document.querySelector('form textarea[name=album]');
-				let name=document.querySelector('form input[name=name]');
-				let review=document.querySelector('form textarea[name=review]');
-				let newReview={
-					name:name.value,
-					review:review.value
-				}
-				let newAlbum={
-					band:band.value,
-					album:album.value,
-					review:newReview.value
-				}
-				database.update(albums.documentID,index,newAlbum);
-			});
+	review: function(index) {
+		database.detail(albums.documentID, index, function(item) {
+		  console.log(item); // Log the entire 'item' object to the console
+	  
+		  document.getElementById('loading').style.display = 'none';
+		  document.getElementById('album-band').innerText = item.band;
+		  document.getElementById('album-text').innerText = item.album;
+		  if (item.review) {
+			document.querySelector('form input[name=name]').value = item.review.name;
+			document.querySelector('form textarea[name=review]').value = item.review.text;
+		  }
+	  
+		  document.querySelector('form').addEventListener('submit', function(e) {
+			e.preventDefault();
+			let band = item.band
+			let album = item.album
+			let name = document.querySelector('form input[name=name]');
+			let review = document.querySelector('form textarea[name=review]');
+			let newReview = {
+			  name: name.value,
+			  text: review.value
+			}
+			if (!item.reviews) {
+			  item.reviews = []; // Create a new 'reviews' array if it doesn't exist
+			}
+			item.reviews.push(newReview); // Add the new review to the 'reviews' array
+			let newAlbum = {
+			  band: band,
+			  album: album,
+			  reviews: item.reviews // Use the updated 'reviews' array in the new album object
+			}
+			database.update(albums.documentID, index, newAlbum);
+		  });
 		});
-	}
+	  }
+	  
+	  
+	  
 }
